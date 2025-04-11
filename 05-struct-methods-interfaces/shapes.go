63f2b1e929cf558ecf6fcfbf25b4c69a8d00c27b
abkolan <104200+abkolan@users.noreply.github.com>
2025-04-11 13:23:51 +0530

    05 lesson complete

--- 05-struct-methods-interfaces/shapes.go
+++ 05-struct-methods-interfaces/shapes.go
@@ -11,6 +11,15 @@ type Rectangle struct {
 	Height float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
+
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
