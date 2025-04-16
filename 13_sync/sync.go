aa13942b3bf61e9ff7464361af1c9e34ad1f9a8c
abkolan <104200+abkolan@users.noreply.github.com>
2025-04-16 13:11:43 +0530

    13 done

--- 13_sync/sync.go
+++ 13_sync/sync.go
@@ -1,1 +1,27 @@
 package _3_sync
+
+import "sync"
+
+type counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+type Counter interface {
+	Inc()
+	Value() int
+}
+
+func (c *counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *counter) Value() int {
+	return c.value
+}
+
+func NewCounter() Counter {
+	return &counter{}
+}
