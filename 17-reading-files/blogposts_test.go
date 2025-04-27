5a34fa517689f4f713c6d49793c64f7007586293
abkolan <104200+abkolan@users.noreply.github.com>
2025-04-27 18:02:35 +0530

    wip "Reading files"

---
+++ 17-reading-files/blogposts_test.go
@@ -0,0 +1,21 @@
+package blogposts_test
+
+import (
+	blogposts "github.com/abkolan/quii/17-reading-files"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("hi")},
+		"hello-world2.md": {Data: []byte("hola")},
+	}
+
+	posts := blogposts.NewPostsFromFS(fs)
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d but wanted %d", len(posts), len(fs))
+	}
+
+}
